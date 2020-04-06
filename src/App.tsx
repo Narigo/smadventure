b6f291377f03c49a0e4b5ef40e56e141e8917987
Joern Bernhardt <joern.bernhardt@compose.us>
2020-04-06 10:14:35 +0200

    Add an image as an asset
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import InteractionScreen from "./component/screen/InteractionScreen";
+import logo from "./asset/logo.png";
 
 export default function App() {
   return (
     <InteractionScreen
-      picture={0}
+      picture={logo}
       description="hello description"
       actions="hello actions"
     />
