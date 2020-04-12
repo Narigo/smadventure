cfa7a18d0994b7a7ec00dd6b2c7651de8a8be619
Joern Bernhardt <joern.bernhardt@compose.us>
2020-04-12 19:46:32 +0200

    Use children directly
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,20 @@ import logo from "./asset/logo.svg";
 import ActionButton from "./component/action-button/ActionButton";
 
 export default function App() {
-  const prev = (
-    <ActionButton
-      label="prev"
-      action={() => {
-        console.log("clicked prev!");
-      }}
-    />
-  );
-  const next = (
-    <ActionButton
-      label="next"
-      action={() => {
-        console.log("clicked next!");
-      }}
-    />
-  );
   return (
     <InteractionScreen picture={logo} description="hello description">
-      {prev}
-      {next}
+      <ActionButton
+        label="prev"
+        action={() => {
+          console.log("clicked prev!");
+        }}
+      />
+      <ActionButton
+        label="next"
+        action={() => {
+          console.log("clicked next!");
+        }}
+      />
     </InteractionScreen>
   );
 }
