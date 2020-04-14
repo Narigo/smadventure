b94594f93149a1e8ce705027d7ea0c290cf50198
Joern Bernhardt <joern.bernhardt@compose.us>
2020-04-15 01:53:26 +0200

    Rename GameScreen to SampleGame
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Game from "./component/game/Game";
-import GameScreen from "./component/sample-game/SampleGame";
+import SampleGame from "./component/sample-game/SampleGame";
 
 export default function App() {
-  return <Game initialScreen={<GameScreen />} />;
+  return <Game initialScreen={<SampleGame />} />;
 }
